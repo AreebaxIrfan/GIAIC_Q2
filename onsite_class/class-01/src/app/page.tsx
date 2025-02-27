c3fa971f26dee5a212efeebdab60de2f09862a98
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-02-27 18:01:37 +0500

    Add files via upload

---
+++ onsite_class/class-01/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Header/Navbar";
+import Hero from "@/components/LandingPage/Hero";
+
+export default function Home() {
+   return (
+      <>
+      <Navbar/>
+        <Hero/>
+      </>
+   );
+}

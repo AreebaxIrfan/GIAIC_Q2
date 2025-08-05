12fef73ca8906acae639df84ff7a07e8d3b06ae5
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-08-05 12:32:52 +0500

    Update page.tsx

--- class_assignment/class-1/src/app/page.tsx
+++ class_assignment/class-1/src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
    return (
-      <>
          <div >
             <div className="h-screen flex items-center justify-center relative bg-black">
                <div className="flex flex-col items-center justify-center border-4 border-yellow-500 p-8 hover:bg-rose-500 transition duration-300">
@@ -13,6 +12,5 @@ export default function Home() {
               
             </div>
          </div>
-      </>
    );
 }
